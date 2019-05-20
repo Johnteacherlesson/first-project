4708db9ec0fbf3aec80f7db0062a4e14461cc90d
Johnteacherlesson <50099086+Johnteacherlesson@users.noreply.github.com>
2019-05-20 18:53:14 +0900

    notes
    REMEMBER THE HW AND 5 MINS EACH DAY!!!!!

---
+++ :/notes.py
@@ -0,0 +1,12 @@
+###i learned function and how to minus lists! and addition and subtraction
+
+player1_hp=10
+player2_hp=10
+player1_hp = player1_hp-5
+print ("player 1 has",player1_hp,"hp!")
+player2_hp = player2_hp-9999999999999999
+player1_hp = player1_hp+9999999999999999
+print ("player 1 has",player1_hp,"hp!")
+print ("player 2 has",player2_hp,"hp!")
+print ("player 2 has been obliterated!")
+

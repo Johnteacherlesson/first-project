d2e55ffa33a93257b0bc3df69fd90e4030ee7313
Johnteacherlesson <50099086+Johnteacherlesson@users.noreply.github.com>
2019-08-19 17:43:48 +0900

    2019_08_19
    I learned global and local variable.  a global variable is a variable outside a function. a local variable is a variable inside a function.
                                                                                                        code is procedural ( its going step by step )

--- lessonNotes.py
+++ lessonNotes.py
@@ -1,5 +1,6 @@
 a=10
 # global variable
+# coding is a procedural langauge
 
 def say10():
     a = 100
@@ -8,4 +9,9 @@ def say10():
     a = 10
     print(a)
     
+    b=19
+    
+    print(b);
+
+
 say10();

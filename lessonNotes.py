b99270d3e41808ccad1f5ecfb6aa44f611b16e41
Johnteacherlesson <50099086+Johnteacherlesson@users.noreply.github.com>
2019-08-19 19:03:07 +0900

    2019-08-19
    I learned how to explain better by learning the terms.   I learned how to make a generator that randomly picks out an item in a list.

--- lessonNotes.py
+++ lessonNotes.py
@@ -15,3 +15,30 @@ def say10():
 
 
 say10();
+
+import random
+#needed for random number generator
+
+def randgen():
+#generate random numbers higher then 50 but lower than 100
+   
+    a=random.randint(0,101)
+    print(a)
+    if a<50:
+       a=a+10 
+       print(a)
+       randgen();
+       
+randgen();
+
+
+import random
+
+
+
+def randLgen():
+    g=["N-NANI!","MUDA","5 mins on the clock","burning time"]
+    b=random.choice(g)
+    print(b)
+
+randLgen();
